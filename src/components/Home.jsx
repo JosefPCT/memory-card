f5d80a020a6f70856431f8a3a78ee9a10ca271bd
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-08-04 20:43:21 +0800

    feat: test getting data from pokemon api

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,43 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export function Home(){
+
+    let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    
+    // const test = async () => {
+    //     const testmore = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    //     const res = await testmore.json();
+    //     console.log(res.sprites.other['official-artwork'].front_default);
+    //     // return res.sprites.other['official-artwork'].front_default;
+    // }
+
+    const [data, setData] = useState(null);
+    useEffect(() => {
+      const fetchData = async() => {
+        try {
+            const resp = await fetch(url);
+            if(!resp.ok){
+                throw new Error(`Error!! {resp.status}`);
+            }
+            const result = await resp.json();
+            setData(result.sprites.other['official-artwork'].front_default);
+        } catch (err){
+            console.log(err);
+        }
+      };
+      fetchData();
+    });
+
+    // let x = test();
+    
+
     return(
-      <h1>Memory Game</h1>
+      <div>
+          <h1>Memory Game</h1>
+          <a href="#"><img src={data} alt="" /></a>
+      </div>
+
+      
     );
 }
